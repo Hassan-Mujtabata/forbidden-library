7e6351011f65e4f90e95e9ea923b9982228bea28
Hassan-Mujtabata <sandstormhellhound@gmail.com>
2026-07-20 15:58:47 +0300

    Patch 3.16 Broken Links (reapplied over cron)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vault-v33";
+const CACHE = "vault-v34";
 const ASSETS = ["./", "./index.html", "./content.enc", "./manifest.json", "./icon.svg",
   "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
