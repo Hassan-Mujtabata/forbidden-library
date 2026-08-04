fe7cd88a5daf1a9a5a570e5ed2426beaf374efed
Hassan-Mujtabata <sandstormhellhound@gmail.com>
2026-08-04 13:49:30 +0400

    3.99: The Update Button Now Goes Away

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vault-v116";
+const CACHE = "vault-v117";
 const ASSETS = ["./", "./index.html", "./content.enc", "./manifest.json", "./icon.svg",
   "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png",
   // #89: precache access/status so the visibility rules and pipeline card survive offline (they're network-first below)
